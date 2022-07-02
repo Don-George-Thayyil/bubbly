917249fe594135759594f932eb5783ee80be6b82
Don George Thayyil <dongeo77@gmail.com>
2022-07-02 14:53:29 +0530

    Changed friction

--- entities/bubble.js
+++ entities/bubble.js
@@ -1,66 +1,74 @@
-import Matter from 'matter-js';
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import Matter from "matter-js";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 //function gives actual viewable body to rigidBody,which we cannot actually see on the screen.
-const Objectify = (props) => { 
+const Objectify = (props) => {
+  const [tap,setTap] = useState(false)
   //setting dimensions and positions(x,y)
   const width = props.body.bounds.max.x - props.body.bounds.min.x;
   const height = props.body.bounds.max.y - props.body.bounds.min.y;
 
-  const xBody = props.body.position.x - width/2;
-  const yBody = props.body.position.y - height/2;
-  
+  const xBody = props.body.position.x - width / 2;
+  const yBody = props.body.position.y - height / 2;
+
   return (
     //stuff below is actually rendered out and has physics of a circular rigid body.
-    <View style = {[styles.bubble, {
-      top: yBody,
-      left: xBody,
-      height: props.body.circleRadius*2,
-      width: props.body.circleRadius*2,
-      borderRadius: props.body.circleRadius,
-      borderWidth:0,
-      flex:1,
-      justifyContent:"center",
-      alignItems:"center"
-}]}
-    >
-      <Text>{props.touchId.touchId}</Text>
-    </View>
-  )
-}
+      <View
+        style={[
+          styles.bubble,
+          {
+            top: yBody,
+            left: xBody,
+            height:props.body.circleRadius * 2,
+            width:props.body.circleRadius * 2,
+            borderRadius:props.body.circleRadius,
+            borderWidth: 0,
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        ]}
+      >
+        <Text>{props.touchId.touchId}</Text>
+      </View>
+  );
+};
 
-
-function Bubble(props) { // we create a rigid body, so that physics is applied
+function Bubble(props) {
+  // we create a rigid body, so that physics is applied
 
   const top = props.position[0];
   const left = props.position[1];
   const radius = props.radius;
   const touchId = props.id;
 
-  const bubble = Matter.Bodies.circle(
-    top, left, radius,{ label: "bubble", frictionAir: 0.005 } 
-  );//circle rigid body
-
+  const bubble = Matter.Bodies.circle(top, left, radius, {
+    label: "bubble",
+    frictionAir: 0.001,
+  }); //circle rigid body
 
   Matter.World.add(props.world, bubble);
 
   //returns the props required in {entities}
   return {
-    body: bubble, 
-    touchId:{touchId},
-    renderer: <Objectify/> //body & touchId will be passed on to the Objectify function.
-  }
+    body: bubble,
+    touchId: { touchId },
+    renderer: <Objectify />, //body & touchId will be passed on to the Objectify function.
+  };
 }
-
 
 const styles = StyleSheet.create({
   bubble: {
-    borderColor: "#fff",
-    borderWidth: 4,
+    borderWidth: 0,
     backgroundColor: "skyblue",
     position: "absolute",
-  }
-})
+  },
+  bubbleHuge: {
+    borderWidth: 0,
+    backgroundColor: "skyblue",
+    position: "absolute",
+  },
+});
 
 export default Bubble;
